28eada216032cd8ee302c53714ff6087a2e2c943
namkhangtb <khangnguyen201201@gmail.com>
2022-11-12 17:52:55 +0700

    up all crud

--- src/router/api.js
+++ src/router/api.js
@@ -1,13 +1,18 @@
 var express = require('express')
-var APIUserController = require('../controller/APIUserController.js');
-var APILoaiSanPhamController = require('../controller/APILoaiSanPhamController.js');
-var ApiChiTietSanPhamController = require('../controller/APIChiTietSanPhamController')
-
+var APIUserController = require('../controller/APIUserController');
+var APILoaiSanPhamController = require('../controller/APILoaiSanPhamController');
+var APIChiTietSanPhamController = require('../controller/APIChiTietSanPhamController')
+var APIMauSacController = require('../controller/APIMauSacController')
+var APIKichCoController = require('../controller/APIKichCoController')
+var APIHoaDonController = require('../controller/APIHoaDonController')
+var APIChiTietHoaDonController = require('../controller/APIChiTietHoaDon')
+var APITinTucController = require('../controller/APITinTucController')
+var APILienHeController = require('../controller/APILienHeController')
 
 let router = express.Router();
 var { conn, config } = require('../configs/connectDB')
 var sql = require('mssql/msnodesqlv8');
-const APIChiTietSanPhamController = require('../controller/APIChiTietSanPhamController');
+
 
 
 const initAPIRoute = (app) => {
@@ -34,9 +39,47 @@ const initAPIRoute = (app) => {
     router.put('/update-product-details', APIChiTietSanPhamController.updateChiTietSanPham);
     router.delete('/delete-product-details/:id', APIChiTietSanPhamController.deleteChiTietSanPham);
 
+    //Mau Sac
+    router.get('/color', APIMauSacController.getAllMauSacs);
+    router.get('/color/:id', APIMauSacController.getOneMauSac);
+    router.post('/create-color', APIMauSacController.createNewMauSac);
+    router.put('/update-color', APIMauSacController.updateMauSac);
+    router.delete('/delete-color/:id', APIMauSacController.deleteMauSac);
 
+    //Kich Co
+    router.get('/size', APIKichCoController.getAllKichCos);
+    router.get('/size/:id', APIKichCoController.getOneKichCo);
+    router.post('/create-size', APIKichCoController.createNewKichCo);
+    router.put('/update-size', APIKichCoController.updateKichCo);
+    router.delete('/delete-size/:id', APIKichCoController.deleteKichCo);
 
+    //Hoa Don
+    router.get('/bill', APIHoaDonController.getAllHoaDons);
+    router.get('/bill/:id', APIHoaDonController.getOneHoaDon);
+    router.post('/create-bill', APIHoaDonController.createNewHoaDon);
+    router.put('/update-bill', APIHoaDonController.updateHoaDon);
+    router.delete('/delete-bill/:id', APIHoaDonController.deleteHoaDon);
 
+    //Chi Tiet Hoa Don
+    router.get('/bill-details', APIChiTietHoaDonController.getAllChiTietHoaDons);
+    router.get('/bill-details/:id', APIChiTietHoaDonController.getOneChiTietHoaDon);
+    router.post('/create-bill-details', APIChiTietHoaDonController.createNewChiTietHoaDon);
+    router.put('/update-bill-details', APIChiTietHoaDonController.updateChiTietHoaDon);
+    router.delete('/delete-bill-details/:id', APIChiTietHoaDonController.deleteChiTietHoaDon);
+
+    //Tin Tuc
+    router.get('/news', APITinTucController.getAllTinTucs);
+    router.get('/news/:id', APITinTucController.getOneTinTuc);
+    router.post('/create-news', APITinTucController.createNewTinTuc);
+    router.put('/update-news', APITinTucController.updateTinTuc);
+    router.delete('/delete-news/:id', APITinTucController.deleteTinTuc);
+
+    //Lien He
+    router.get('/contact', APILienHeController.getAllLienHes);
+    router.get('/contact/:id', APILienHeController.getOneLienHe);
+    router.post('/create-contact', APILienHeController.createNewLienHe);
+    router.put('/update-contact', APILienHeController.updateLienHe);
+    router.delete('/delete-contact/:id', APILienHeController.deleteLienHe);
 
 
 
